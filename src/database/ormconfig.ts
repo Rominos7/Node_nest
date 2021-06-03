42ef2f8649910c437ef0a92545db51bdd816ab8d
Roman Serhiychuk <Rominos7@gmail.com>
2021-06-03 13:54:32 +0300

    Changed ormconfig

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -4,13 +4,14 @@ export const config: TypeOrmModuleOptions = {
     type: 'postgres',
     host: 'localhost',
     port: 5432,
-    username: 'admin',
+    username: 'postgres',
     password: 'admin',
     database: 'task_manager',
-    entities:  [__dirname + '/**/*.entity.ts'],
+    entities:  [__dirname + '/entities/*.entity.ts'],
     migrations: ['src/migration/*{.ts}'],
     schema: 'public',
     cli: {
       migrationsDir: 'src/migration'
-    }
+    },
+    synchronize: true,
   }
