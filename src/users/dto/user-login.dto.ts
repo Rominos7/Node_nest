5ae64ac7e5eb0f6eaa2e6b1169f0cd46da9bdde0
Roman Serhiychuk <Rominos7@gmail.com>
2021-07-22 01:54:17 +0300

    Changed DTO

--- src/users/dto/user-login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class UserLoginDto {
-    @IsNotEmpty()
-    @IsString()
-    @Type((value) => () => String(value))
-    name: string;
-  }
--- src/users/dto/user-password.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class UserPasswordDto {
-    @IsNotEmpty()
-    @IsString()
-    @Type((value) => () => String(value))
-    password: string;
-  }
