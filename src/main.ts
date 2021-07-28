f2148a62cba2bf1d8c937c65c2f8ee6bd429dc99
Roman Serhiychuk <Rominos7@gmail.com>
2021-07-29 02:01:26 +0300

    Start working with passport

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } }));
+
+  const configService = app.get(ConfigService);
+ 
+  app.use(
+    session({
+      secret: configService.get('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+ 
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //TODO: aplly mniddleware for root redirection
+  //app.use('/app/user/', name_of_the_controller)
+
   await app.listen(3000);
 }
 bootstrap();
